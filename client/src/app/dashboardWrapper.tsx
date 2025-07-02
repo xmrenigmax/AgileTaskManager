61e98a2ad36c8be19b994e9f1b3211ec067c12d0
xmrenigmax <RileyJordan21@hotmail.com>
2025-07-02 19:11:41 +0100

    Update Navbar Search Bar and Settings icon

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import Sidebar from "@/app/(components)/Sidebar"
 import Navbar from "@/app/(components)/Navbar";
+
 
 
 
@@ -7,6 +9,7 @@ const DashboardWrapper = ( {children}: {children: React.ReactNode}) => {
   return (
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
         {/* Sidebar */}
+        <Sidebar/>
         <main className={'flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64'}>
 
             {/* Navbar */}
