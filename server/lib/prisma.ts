6831de5f0421c090599f697ba15efb80788b619a
xmrenigmax <RileyJordan21@hotmail.com>
2025-08-09 23:37:06 +0100

    Update : using prisma built in data-proxy

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
