14ff9f9b81bb27a8112886c91076a7f58c43c464
xmrenigmax <RileyJordan21@hotmail.com>
2025-08-10 15:55:37 +0100

    Update : so far this is nearly the best result

--- server/api/index.ts
+++ server/api/index.ts
@@ -3,13 +3,6 @@ import serverless from 'serverless-http';
 import { execSync } from 'child_process';
 import apiApp from './app';
 
-// Generate Prisma Client if not already done (for Data Proxy)
-try {
-  execSync('prisma generate --data-proxy', { stdio: 'inherit' });
-} catch (error) {
-  console.error('Failed to generate Prisma client:', error);
-}
-
 const handler = serverless(apiApp);
 
 export default (req: VercelRequest, res: VercelResponse) => {
