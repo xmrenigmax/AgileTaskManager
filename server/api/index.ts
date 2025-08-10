f85f1d6f1aa2dd110d1c434858afb56468bfbdcc
xmrenigmax <RileyJordan21@hotmail.com>
2025-08-10 16:26:13 +0100

    Update : so far this is nearly the best result

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,10 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import serverless from 'serverless-http';
-import { execSync } from 'child_process';
+import { PrismaClient } from '@prisma/client/edge'; // Use Edge Client for Data Proxy
 import apiApp from './app';
+
 
 const handler = serverless(apiApp);
 
-export default (req: VercelRequest, res: VercelResponse) => {
+export default async (req: VercelRequest, res: VercelResponse) => {
   return handler(req, res);
 };
